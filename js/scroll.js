6a861e002c3565b360aee3d1f357bdc5737ce719
Asaf Arad <65238226+asafarad1@users.noreply.github.com>
2023-02-19 01:57:52 +0200

    first_commit

---
+++ js/scroll.js
@@ -0,0 +1,54 @@
+// control parameters //
+let leg_factor = 0.1;
+let threshold = 40;
+let tickTimeout_time = 150;
+
+// general parameters //
+let lastKnownScrollPosition = 0;
+let diff = 0;
+let R_leg_factor = 1;
+let G_leg_factor = 1 + leg_factor;
+let B_leg_factor = 1 + (2 * leg_factor);
+let leg_R = 0;
+let leg_G = 0;
+let leg_B = 0;
+let tickTimeout = null;
+let ticking = false;
+
+const R = document.querySelectorAll(".R");
+const G = document.querySelectorAll(".G");
+const B = document.querySelectorAll(".B");
+
+function channel_leg(scrollDiff) {
+    leg_R = (scrollDiff * R_leg_factor).toString()+"px";
+    leg_G = (scrollDiff * G_leg_factor).toString()+"px";
+    leg_B = (scrollDiff * B_leg_factor).toString()+"px";
+    R.forEach((userItem) => {
+      userItem.style.top = leg_R;
+    });
+    G.forEach((userItem) => {
+      userItem.style.top = leg_G;
+    });
+    B.forEach((userItem) => {
+      userItem.style.top = leg_B;
+    });
+}
+
+document.addEventListener("scroll", (event) => {
+  diff = window.scrollY - lastKnownScrollPosition;
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      clearTimeout( tickTimeout );
+      if (Math.abs(diff) >= threshold) {
+        channel_leg(-diff);
+        tickTimeout = setTimeout( () => channel_leg( 0 ), tickTimeout_time );
+      } else {
+        channel_leg(0);
+      }
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
