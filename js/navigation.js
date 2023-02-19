decdaaa1a2774b1625ef45c01442450cf7cdd765
Asaf Arad <65238226+asafarad1@users.noreply.github.com>
2023-02-19 18:58:43 +0200

    ARTICLE_BUTTLONS WIP

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,17 @@
-const a2_title = document.getElementById('a2_title');
 
-window.scroll(function() {
-    var hT = a2_title.offset().top,
-        hH = a2_title.outerHeight(),
-        wH = window.height(),
-        wS = whit.scrollTop();
-     console.log((hT-wH) , wS);
-    if (wS > (hT+hH-wH)){
-      alert('you have scrolled to the h1!');
-    }
- });
+const a2_title = document.getElementById('a2_title');
+const a1_button = document.getElementById('a1_button');
+
+var initialTopOffset = $('.headline').offset().top;
+window.scroll(function(event) {
+  var scroll = window.scrollTop();
+  // if the top of our browser is inside the section2
+  if (scroll + initialTopOffset >= a2_title.offset().top && scroll + initialTopOffset <= a2_title.offset().top + a2_title.outerHeight()) {
+    a1_button.addClass('in_a1'); // add the class
+  } else {
+    a1_button.removeClass('in_a1'); // remove the class
+  }
+});
+
+//trigger the scroll
+$(window).scroll(); //ensure if you're in current position when page is refreshed
