a4b3c632306b059c018029c2c99919f448feb7ad
Asaf Arad <65238226+asafarad1@users.noreply.github.com>
2023-02-22 14:13:11 +0200

    load

---
+++ js/spirit.js
@@ -0,0 +1,28 @@
+// const circle_R = document.querySelector('.circle_R')
+// const circle_G = document.querySelector('.circle_G')
+// const circle_B = document.querySelector('.circle_B')
+
+// const body_style = document.querySelector('body').style;
+
+
+// circle_R.addEventListener("mouseover", function() {
+//     body_style.backgroundColor = 'var(--R-clr)';
+//     body_style.mixBlendMode = 'normal';
+//     body_style.zIndex = '10000';
+// }); 
+  
+// circle_R.addEventListener("mouseout", function() {
+//     body_style.backgroundColor = 'var(--black-clr)';
+//     body_style.mixBlendMode = 'screen';
+//     body_style.zIndex = '0';
+// });
+
+const header_R = document.querySelector('.header_R')
+const header_G = document.querySelector('.header_G')
+const header_B = document.querySelector('.header_B')
+
+let tickTimeout_time = 150;
+
+window.addEventListener('load', () => {
+    
+});
