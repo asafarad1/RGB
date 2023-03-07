856b6145eca5d88908d43d36346b1545be46124d
Asaf Arad <65238226+asafarad1@users.noreply.github.com>
2023-03-07 03:37:10 +0200

    a

--- js/RGB_buttons.js
+++ js/RGB_buttons.js
@@ -1,20 +1,20 @@
 document.getElementById('R_button').addEventListener("click", function() {
-    this.classList.toggle('clicked');
-    R.forEach((userItem) => {
-        userItem.classList.toggle('channelSwitch');
-      });
+  this.classList.toggle('clicked');
+  R.forEach((userItem) => {
+      userItem.classList.toggle('channelSwitch');
+    });
 })
 
 document.getElementById('G_button').addEventListener("click", function() {
-    this.classList.toggle('clicked');
-    G.forEach((userItem) => {
-        userItem.classList.toggle('channelSwitch');
-      });
+  this.classList.toggle('clicked');
+  G.forEach((userItem) => {
+      userItem.classList.toggle('channelSwitch');
+    });
 })
 
 document.getElementById('B_button').addEventListener("click", function() {
-    this.classList.toggle('clicked');
-    B.forEach((userItem) => {
-        userItem.classList.toggle('channelSwitch');
-      });
+  this.classList.toggle('clicked');
+  B.forEach((userItem) => {
+      userItem.classList.toggle('channelSwitch');
+    });
 })
